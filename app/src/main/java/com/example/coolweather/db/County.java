2d9dee0c1faa486c85e51e450be0c515b798e5f9
yaoliangyong <yaoliangyong221@163.com>
2018-11-17 16:47:27 +0800

    yly 3

--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -1,10 +1,47 @@
 package com.example.coolweather.db;
 
-import DataSupport;
+import org.litepal.crud.DataSupport;
+
 
 /**
  * Created by yly on 2018/11/17.
  */
 
 public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
 }
