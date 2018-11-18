e8271d22e7fb432b8115317e5e207ce438c79fa3
yaoliangyong <yaoliangyong221@163.com>
2018-11-18 10:45:03 +0800

    11.18 yly 2

---
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,11 @@
+package com.example.coolweather;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yly on 2018/11/18.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+    
+}
