1053f46c92469f4ebcc7f1bc6c10b279e0e4c146
yaoliangyong <yaoliangyong221@163.com>
2018-11-19 22:58:56 +0800

    11.19 yly 6

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -1,13 +1,64 @@
 package com.example.coolweather;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.coolweather.gson.Weather;
+import com.example.coolweather.util.Utility;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private ScrollView weatherLayout;
+    private TextView titleCity;
+    private TextView titleUpdateTile;
+    private TextView degreeText;
+    private TextView weatherInfoText;
+    private LinearLayout forecastLayout;
+    private TextView aqiText;
+    private TextView pm25Text;
+    private TextView comfortText;
+    private TextView carWashText;
+    private TextView sportText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        weatherLayout=findViewById(R.id.weather_layout);
+        titleCity=findViewById(R.id.title_city);
+        titleUpdateTile=findViewById(R.id.title_update_time);
+        degreeText=findViewById(R.id.degree_text);
+        weatherInfoText=findViewById(R.id.weather_info_text);
+        forecastLayout=findViewById(R.id.forecast_layout);
+        aqiText=findViewById(R.id.aqi_text);
+        pm25Text=findViewById(R.id.pm25_text);
+        comfortText=findViewById(R.id.comfort_text);
+        carWashText=findViewById(R.id.car_cash_text);
+        sportText=findViewById(R.id.sport_text);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString =prefs.getString("weather",null);
+        if (weatherString!=null){
+            Weather weather= Utility.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        }else{
+            String weatherId=getIntent().getStringExtra("weather_id");
+            weatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
+        }
+
+    }
+
+    private void requestWeather(final String weatherId) {
+
+    }
+
+    private void showWeatherInfo(Weather weather) {
     }
 }
